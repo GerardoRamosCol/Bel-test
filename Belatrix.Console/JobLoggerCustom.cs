2cb61ed261d48c73c56c704dbac327e203228597
Gerardo Ramos <lgerardoramos@hotmail.com>
2018-08-28 18:01:08 -0500

    Modificacion usando patron Factory

---
+++ Belatrix.Console/JobLoggerCustom.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+
+public abstract class LogItem
+{
+    private string _mensaje;
+
+    public string Mensaje
+    {
+        get { return _mensaje; }
+        set { _mensaje = value; }
+    }
+
+    private int _tipo;
+
+    public int Tipo
+    {
+        get { return _tipo; }
+        set { _tipo = value; }
+    }
+
+
+    public abstract string SaveLog();
+
+}
+
+/// <summary>
+/// Clase concreta para el almacenamiento el base de datos
+/// </summary>
+public class SaveDataBase : LogItem
+{
+    public override string SaveLog()
+    {
+        /*Se guarda en base de datos*/
+        SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionStringLogDB"].ConnectionString);
+        SqlCommand command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = "UP_INSERT_LOG";
+        command.CommandType = System.Data.CommandType.StoredProcedure;
+
+        SqlParameter parMsg = new SqlParameter();
+        parMsg.ParameterName = "@MESSAGE";
+        parMsg.Value = Mensaje;
+
+        SqlParameter parType = new SqlParameter();
+        parType.ParameterName = "@TYPE";
+        parType.Value = Tipo;
+
+        command.Parameters.Add(parMsg);
+        command.Parameters.Add(parType);
+
+        try
+        {
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            StringBuilder sbError = new StringBuilder();
+
+            sbError.Append("Error: ");
+            sbError.Append(ex.Message);
+            return sbError.ToString();
+
+        }
+        finally
+        {
+            if (connection.State != System.Data.ConnectionState.Closed)
+                connection.Close();
+        }
+        return "Ok";
+    }
+}
+
+/// <summary>
+/// Clase concreta para el almacenamiento el archivo
+/// </summary>
+public class SaveFile : LogItem
+{
+    public override string SaveLog()
+    {
+        /*Se guarda en archivo*/
+        StringBuilder logText = new StringBuilder();
+        StringBuilder sbFileName = new StringBuilder();
+        sbFileName.Append(System.Configuration.ConfigurationManager.AppSettings["LogFileDirectory"]);
+        sbFileName.Append("LogFile-");
+        sbFileName.Append(DateTime.Now.ToShortDateString().Replace("/", "-"));
+        sbFileName.Append(".txt");
+
+        logText.Append(DateTime.Now.ToShortDateString());
+        logText.Append("TYPE: ");
+        logText.Append(Tipo.ToString());
+        logText.Append(Mensaje);
+
+        try
+        {
+            if (!File.Exists(sbFileName.ToString()))
+                File.Create(sbFileName.ToString()).Dispose();
+
+            using (var sw = new StreamWriter(sbFileName.ToString(), true))
+            {
+                sw.WriteLine(logText.ToString());
+            }
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+
+        return "Ok";
+    }
+}
+
+/// <summary>
+/// Clase factoria para los objetos LogItem
+/// </summary>
+public class LogFactory
+{
+    public static LogItem getLogItem(string tipo)
+    {
+        if (tipo.Equals("BD"))
+            return new SaveDataBase();
+        else
+            return new SaveFile();
+    }
+}
---
+++ Belatrix.Console/JobLoggerCustomTestCase.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace Belatrix.Console
+{
+    [TestFixture]
+    class JobLoggerCustomTestCase
+    {
+        enum LogType { Message = 1, Warning, Error };
+
+        [TestCase]
+        public void SaveOnFile()
+        {
+            StringBuilder messageTest = new StringBuilder();
+            messageTest.Append("MESSAGE FILE TEST # ");
+
+            Random rnd = new Random();
+            messageTest.Append(rnd.Next(1, 500).ToString());
+
+            LogItem LI = LogFactory.getLogItem("FILE");
+            LI.Mensaje = messageTest.ToString();
+            LI.Tipo = (int)LogType.Error; //Para el caso se envia un error
+
+            Assert.AreEqual("Ok", LI.SaveLog());
+        }
+
+        [TestCase]
+        public void SaveOnDatabase()
+        {
+            StringBuilder messageTest = new StringBuilder();
+            messageTest.Append("MESSAGE DB TEST # ");
+
+            Random rnd = new Random();
+            messageTest.Append(rnd.Next(1, 500).ToString());
+
+            LogItem LI = LogFactory.getLogItem("BD");
+            LI.Mensaje = messageTest.ToString();
+            LI.Tipo = (int)LogType.Warning; //Para el caso se envia un warning
+
+            Assert.AreEqual("Ok", LI.SaveLog());
+        }
+    }
+}
